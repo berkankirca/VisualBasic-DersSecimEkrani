2b6c5b74acb85b3daf6ea9e72d96598e953374bc
Berkan Kırca <berkankirca@hotmail.com>
2021-03-16 20:04:22 +0300

    Ders Seçim Ekranı

---
+++ Ders Seçim/WindowsApplication5/Form1.Designer.vb
@@ -0,0 +1,277 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.ListBox1)
+        Me.GroupBox2.Location = New System.Drawing.Point(262, 11)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(176, 162)
+        Me.GroupBox2.TabIndex = 11
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Açılan Dersler"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(6, 19)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(157, 134)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(454, 53)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(87, 23)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = ">>>>"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(454, 91)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(87, 23)
+        Me.Button2.TabIndex = 13
+        Me.Button2.Text = "<<<<"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.ListBox2)
+        Me.GroupBox3.Location = New System.Drawing.Point(561, 12)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(172, 161)
+        Me.GroupBox3.TabIndex = 15
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Seçilen Dersler"
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(6, 19)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(160, 134)
+        Me.ListBox2.TabIndex = 21
+        '
+        'Button3
+        '
+        Me.Button3.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Button3.BackColor = System.Drawing.Color.Red
+        Me.Button3.Location = New System.Drawing.Point(271, 189)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(166, 40)
+        Me.Button3.TabIndex = 16
+        Me.Button3.Text = "ONAYLA"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(112, 249)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(634, 13)
+        Me.Label1.TabIndex = 17
+        Me.Label1.Text = "En fazla 6 ders seçebilir. Alttan ve üstten ders seçme sınırı yoktur. Seçmeli der" &
+    "sten en fazla 1 tane seçilebilir.  "
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(577, 216)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 13)
+        Me.Label2.TabIndex = 18
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(663, 216)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 13)
+        Me.Label3.TabIndex = 20
+        Me.Label3.Text = "Label3"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(2, 19)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(61, 17)
+        Me.RadioButton1.TabIndex = 21
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "1.Sınıf"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(2, 42)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(61, 17)
+        Me.RadioButton2.TabIndex = 22
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "2.Sınıf"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(2, 64)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(61, 17)
+        Me.RadioButton3.TabIndex = 23
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "3.Sınıf"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(2, 87)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(61, 17)
+        Me.RadioButton4.TabIndex = 24
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "4.Sınıf"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.AutoSize = True
+        Me.RadioButton5.Location = New System.Drawing.Point(2, 110)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Size = New System.Drawing.Size(100, 17)
+        Me.RadioButton5.TabIndex = 25
+        Me.RadioButton5.TabStop = True
+        Me.RadioButton5.Text = "Seçmeli Grup"
+        Me.RadioButton5.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Controls.Add(Me.RadioButton5)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton4)
+        Me.GroupBox1.Controls.Add(Me.RadioButton3)
+        Me.GroupBox1.Location = New System.Drawing.Point(28, 31)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(181, 143)
+        Me.GroupBox1.TabIndex = 26
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Dersler"
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 1000
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(558, 189)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(45, 13)
+        Me.Label4.TabIndex = 27
+        Me.Label4.Text = "Label4"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoValidate = System.Windows.Forms.AutoValidate.Disable
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(859, 298)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Name = "Form1"
+        Me.Text = "ÖBS"
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Ders Seçim/WindowsApplication5/Form1.vb
@@ -0,0 +1,229 @@
+﻿
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Enabled = True
+        Label2.Text = "Kalan Süreniz:"
+        Label4.Text = ""
+        Dim t As Integer = 120 'sayacın başlayacağı saniyeyi gösteren değişken
+        Label3.Text = t
+
+
+    End Sub
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Label3.Text = CInt(Label3.Text) - 1 'ders seçimi için kalan süreyi belirleyen timer
+        If Label3.Text = 0 Then
+            Timer1.Enabled = False
+            Dim cevap As Integer
+            cevap = MsgBox("Ders Seçimi İçin Süreniz Doldu.Ek Süre İster Misiniz?", vbYesNo + 32, "ÖBS")
+            If cevap = vbYes Then
+
+                Dim t As Integer = 120
+                Label3.Text = t
+                Timer1.Enabled = True
+            Else
+                MsgBox("Ders Seçiminiz Hayırlı Olsun",, "ÖBS")
+                Me.Close()
+
+
+
+            End If
+
+
+        End If
+    End Sub
+
+    Private Sub RadioButton4_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton4.CheckedChanged
+        ListBox1.Items.Clear()
+        ListBox1.Items.Add("ders4_1")
+        ListBox1.Items.Add("ders4_2")
+        ListBox1.Items.Add("ders4_3")
+        ListBox1.Items.Add("ders4_4")
+        Label4.Text = "Ders Seçimi Onaylanmamıştır!"
+        Label3.Text = 120
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged_1(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        ListBox1.Items.Clear()
+        ListBox1.Items.Add("ders2_1")
+        ListBox1.Items.Add("ders2_2")
+        ListBox1.Items.Add("ders2_3")
+        ListBox1.Items.Add("ders2_4")
+        Label4.Text = "Ders Seçimi Onaylanmamıştır!"
+        Label3.Text = 120
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged_1(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        ListBox1.Items.Clear()
+        ListBox1.Items.Add("ders1_1")
+        ListBox1.Items.Add("ders1_2")
+        ListBox1.Items.Add("ders1_3")
+        ListBox1.Items.Add("ders1_4")
+        Label4.Text = "Ders Seçimi Onaylanmamıştır!"
+        Label3.Text = 120
+
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        ListBox1.Items.Clear()
+        ListBox1.Items.Add("ders3_1")
+        ListBox1.Items.Add("ders3_2")
+        ListBox1.Items.Add("ders3_3")
+        ListBox1.Items.Add("ders3_4")
+        Label4.Text = "Ders Seçimi Onaylanmamıştır!"
+        Label3.Text = 120
+    End Sub
+
+    Private Sub RadioButton5_CheckedChanged_1(sender As Object, e As EventArgs) Handles RadioButton5.CheckedChanged
+        ListBox1.Items.Clear()
+        ListBox1.Items.Add("seçmeli_1")
+        ListBox1.Items.Add("seçmeli_2")
+        ListBox1.Items.Add("seçmeli_3")
+        ListBox1.Items.Add("seçmeli_4")
+        Label4.Text = "Ders Seçimi Onaylanmamıştır!"
+        Label3.Text = 120
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim sorgu As String
+        Dim sorgu2 As String
+        Dim sorgu3 As String
+        Dim sorgu4 As String
+        sorgu = "seçmeli_1"
+        sorgu2 = "seçmeli_2"
+        sorgu3 = "seçmeli_3"
+        sorgu4 = "seçmeli_4"
+        Label3.Text = 120
+
+        If ListBox1.SelectedIndex < 0 Then 'ders seçilmeden ekle butonuna tıklandığında olacak hata
+
+            Label4.Text = "Lütfen Ders Seçiniz!"
+        Else
+            If RadioButton5.Checked = True Then
+                If ListBox2.Items.Contains(sorgu) Then
+
+                    Label4.Text = "Seçmeli Dersten En Fala 1 Tane Seçebilirsin!"
+                ElseIf ListBox2.Items.Contains(sorgu2) Then
+
+                    Label4.Text = "Seçmeli Dersten En Fala 1 Tane Seçebilirsin!"
+                ElseIf ListBox2.Items.Contains(sorgu3) Then
+
+                    Label4.Text = "Seçmeli Dersten En Fala 1 Tane Seçebilirsin!"
+                ElseIf ListBox2.Items.Contains(sorgu4) Then
+
+                    Label4.Text = "Seçmeli Dersten En Fala 1 Tane Seçebilirsin!"
+
+                Else
+                    ListBox2.Items.Add(ListBox1.SelectedItem)
+                    Dim x As String
+                    x = ListBox1.SelectedItem
+                    Dim y As String
+                    y = " dersi eklenmiştir!"
+                    Label4.Text = x + y
+                    Timer2.Enabled = True
+                End If
+
+            Else
+                If ListBox2.Items.Contains(ListBox1.Text) Then ' aynı ders bir daha seçilmek 
+                    Label4.Text = "Seçilmiş Bir Ders!"          'istenirse gösterilecek hata
+
+                ElseIf ListBox2.Items.Count >= 6 Then '6 dersten fazla seçim yapamazsınız uyarısı
+
+                    Label4.Text = "En Fazla 6 Ders Seçimi Yapılabilir!"
+
+                Else
+
+                    Dim x As String
+                    x = ListBox1.SelectedItem
+                    Dim y As String
+                    y = " dersi eklenmiştir!"
+                    ListBox2.Items.Add(ListBox1.SelectedItem)
+                    Label4.Text = x + y
+                    Timer2.Enabled = True
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim seçili As String
+        seçili = ListBox2.SelectedIndex
+        If ListBox2.SelectedIndex < 0 Then
+            Label4.Text = "Lütfen Ders Seçiniz"
+        Else
+            Label4.Text = ListBox2.Items(seçili) + " dersi çıkartılmıştır!"
+            ListBox2.Items.RemoveAt(ListBox2.SelectedIndex)
+            Timer2.Enabled = True
+        End If
+        Label3.Text = 120
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Dim sonuç As String
+        sonuç = CStr(Button3.Text)
+        If sonuç = "ONAYI KALDIR" Then 'onayı kaldırdıktan sonra yapılacak işlemler
+            Label3.Text = 120
+            Timer1.Enabled = True
+            Timer2.Enabled = True
+            GroupBox1.Enabled = True
+            ListBox1.Enabled = True
+            ListBox2.Enabled = True
+            Button1.Enabled = True
+            Button2.Enabled = True
+            Label4.Text = "Ders Seçimi Onaylanmamıştır"
+            Button3.Text = "ONAYLA"
+        Else
+            Timer1.Enabled = False 'onayla butonuna basıldığında yapılacak işlemler
+            Timer2.Enabled = False
+            GroupBox1.Enabled = False
+            ListBox1.Enabled = False
+            ListBox2.Enabled = False
+            Button1.Enabled = False
+            Button2.Enabled = False
+            Label4.Text = "Ders Seçimi Onaylanmıştır"
+            Button3.Text = "ONAYI KALDIR"
+
+        End If
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Timer2.Interval = 3000
+        Dim başlangıç As Integer = 0 '
+        başlangıç = başlangıç + 1
+
+        If başlangıç = 1 Then
+            Label4.Text = "Ders Seçimi Onaylanmamıştır!"
+            Timer2.Enabled = False
+        End If
+
+        'ders seçimi bilgisi verildikten sonra aktif olan timer.
+        '3 saniye sonra ders seçiminin onaylanmadığını bildirecek.
+        'onayla butonuna basılma durumunda enable=false olacak
+
+    End Sub
+
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        Label3.Text = 120
+    End Sub
+
+    Private Sub ListBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox2.SelectedIndexChanged
+        Label3.Text = 120
+    End Sub
+
+    Private Sub Form1_Click(sender As Object, e As EventArgs) Handles Me.Click
+        Label3.Text = 120
+    End Sub
+
+
+    Private Sub Form1_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+
+    End Sub
+End Class
